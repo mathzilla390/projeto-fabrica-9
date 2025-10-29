501ea1a7d66a58d16a8ed9352df2ecfd8fa2d479
mathzilla390 <matheuslimadasilva251@gmail.com>
2025-10-29 16:09:10 -0300

    Update at-9.py

--- at-9.py
+++ at-9.py
@@ -1,25 +1,25 @@
-from flask import Flask, jsonify
-
+from flask import Flask, jsonify,request
 app = Flask(__name__)
 
-itens = [
-    {"id": 1, "nome": "Arroz 5kg", "quantidade": 2, "categoria": "Alimentos", "prioridade": "alta", "comprado": False},
-    {"id": 2, "nome": "Feijão 1kg", "quantidade": 10, "categoria": "Alimentos", "prioridade": "alta", "comprado": False}
-]
+itens = [{"id": 1, "nome": "Arroz 5kg", "quantidade": 2, "categoria": "Alimentos", "prioridade": "alta", "comprado": False},
+    {"id": 2, "nome": "Feijão 1kg", "quantidade": 10, "categoria": "Alimentos", "prioridade": "alta", "comprado": False}]
 
 @app.route("/items", methods=["GET"])
 def get_items():
-    """Retorna todos os itens da lista."""
     return jsonify({"itens": itens, "total": len(itens)}), 200
 
-
-@app.route("/items/<int:id>", methods=["GET"])
-def get_item_by_id(id):
-    """Retorna um item específico pelo ID."""
+@app.route("/items/<int:id>", methods=["PUT"])
+def update_item(id):
+    dados = request.get_json()
     for item in itens:
         if item["id"] == id:
-            return jsonify({"mensagem": "Item encontrado!", "item": item}), 200
+            item.update(dados)
+            return jsonify({"mensagem": "Item atualizado com sucesso!", "item": item}), 200
+
     return jsonify({"erro": "Item não encontrado!"}), 404
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route("/items/<int:id>", methods=["DELETE"])
+def delete_item(id):
+    if item['id'] == id:
+        return jsonfy({'mensage': 'item deletado'})
+    return jsofy({'erro':'item não encontrado'}),404
